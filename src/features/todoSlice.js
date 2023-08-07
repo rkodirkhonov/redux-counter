b5cb6c255c99198fe388b0321857fe82358f4cd0
rkodirkhonov <kodirkhonov99@gmail.com>
2023-08-07 23:40:05 +0900

    Added new project files

---
+++ src/features/todoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let nextTodoId = 0;
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nextTodoId++,
+        text: action.payload,
+        completed: false,
+      };
+      state.push(newTodo);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    removeTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
